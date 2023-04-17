270a65ea7663fafe76b5a9f665244c8a9a959be8
239055 <zdenek.kindl.junior@gmail.com>
2023-04-17 08:56:55 +0200

    push 2

--- sorting.py
+++ sorting.py
@@ -24,10 +24,28 @@ def read_data(file_name):
         return data
 
 
+def selection_sort(seznam_cisel):
+    """
+
+    :param seznam_cisel:
+    :return:
+    """
+    n = len(seznam_cisel)
+    for i in range(n - 1):
+        min_idx = i
+        for j in range(i + 1, n):
+            if seznam_cisel[j] < seznam_cisel[min_idx]:
+                min_idx = j
+        seznam_cisel[i], seznam_cisel[min_idx] = seznam_cisel[min_idx], seznam_cisel[i]
+    return seznam_cisel
+
+
 def main():
     data = read_data('numbers.csv')
     print(data["series_3"])
-
+    seznam = [1058, 998, 1235, 1147, 3365, 7741, 1011, 6555, 666, 6666, 7777]
+    serazeni = selection_sort(seznam)
+    print(serazeni)
 
 if __name__ == '__main__':
     main()
